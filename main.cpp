880b1dddd58b3d5b012dca5daf26cb30c524ad26
Hotik <madscientist996@gmail.com>
2016-02-22 17:46:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -96,7 +96,7 @@ void read_cb(struct ev_loop *loop, struct ev_io *watcher, int revents)
 		
 //	} else cout << "parsing error";
 
-    	cout << buffer;
+    	printf("%s", buffer);
     	send(watcher->fd, not_found, sizeof(not_found), MSG_NOSIGNAL);
        // send(watcher->fd, buffer, r, MSG_NOSIGNAL);
 
