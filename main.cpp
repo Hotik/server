cbdf9360a619ca547169bf7d65f5df5fd665bcbe
Hotik <madscientist996@gmail.com>
2016-02-23 18:47:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,18 +19,16 @@
 using namespace std;
 
 static const char* templ = "HTTP/1.0 200 OK\r\n"
-
 		           "Content-length: %d\r\n"
-
 		       	   "Connection: close\r\n"
-
 		       	   "Content-Type: text/html\r\n"
-
 		       	   "\r\n"
-
 		       	   "%s";
 		       	   
- static const char *not_found = "HTTP/1.0 404 NOT FOUND\r\nContent-Type: text/html\r\n\r\n";
+ static const char *not_found = "HTTP/1.0 404 NOT FOUND\r\n"
+ 				"Connection: close\r\n"
+ 				"Content-Type: text/html\r\n"
+ 				"\r\n";
 
     char* dir;
 
